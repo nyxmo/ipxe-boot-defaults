0087217d7e13a4e18bea949aada9da6386552552
Nicolas Wild <nwild@nyxmo.com>
2018-05-07 18:22:41 +0200

    Added all targets to build/release

--- build.sh
+++ build.sh
@@ -4,8 +4,7 @@
 echo "CFLAGS	+= -fno-pie" >> arch/x86/Makefile.pcbios
 echo "LDFLAGS	+= -no-pie" >> arch/x86/Makefile.pcbios
 
-make bin/ipxe.iso EMBED=/ipxe/src/defaults
-#make all EMBED=defaults.ipxe
+make all EMBED=/ipxe/src/defaults
 
 rm bin/*.o
 rm bin/*.a
